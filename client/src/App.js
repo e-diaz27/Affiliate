9fcc0e4cfc3c00edcac0ffb4801f9f720e04c791
TheJaguarKing <eddie.diaz@me.com>
2022-04-19 20:00:31 -0500

    created front-end component template

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+
+
+//components
+import Navbar from "./components/navbar";
+import RecordList from "./components/recordList";
+import Edit from "./components/edit";
+import Create from "./components/create";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Dietary Supplement Offers</header>
+      <Navbar />
+        <Routes>
+          <Route exact path="/" element={<RecordList />} />
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/create" element={<Create />} />
+        </Routes>
     </div>
   );
 }
